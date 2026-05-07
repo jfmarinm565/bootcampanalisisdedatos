4479122acb4bb5eb68c8574c3759c1b8e445f507
jfmarinm565 <jfmarinm@gmail.com>
2026-05-06 21:34:51 -0500

    cp-app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import pandas as pd
 
+import requests
+import pandas as pd
+from io import StringIO
+
+
+
+
 ruta_zni = 'https://github.com/juliandariogiraldoocampo/analisis_taltech/raw/refs/heads/main/explorador/ZNI.csv'
-df = pd.read_csv(ruta_zni)
+response = requests.get(ruta_zni, verify=False)
+
+df = pd.read_csv(StringIO(response.text))
 
 st.header('Estado de la Prestación del Servicio de Energía')
 st.subheader('Zonas no Interconectadas (ZNI)')
